272f23bbc6338a652899e75a58c1d8bbee85c405
AlexWilliamBr <alexwillian.a.silva@gmail.com>
2025-02-08 13:12:00 -0300

    exclusao de banco de dados, vou continuar a testar na outra versao

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 import express  from 'express'
-import sqlite3 from "sqlite3";
 
-const db =  new sqlite3.Database('./banco.db')
-
-db.run(`CREATE TABLE IF NOT EXISTS cadrasto ( id INTEGER PRIMARY KEY  AUTOINCREMENT, nome TEXT NOT NULL, email TEXT NOT NULL, idade TEXT NOT NULL)`)
 
 
 
